4e4995adc03b804097a8aa26a0e1e712ff59bc78
Tolga Sarikaya <113526329+TolgaSarikayaa@users.noreply.github.com>
2024-07-16 13:11:49 +0300

    Hackathon assignment 2 has started

---
+++ CalorieCook/CalorieCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCookApp.swift
+//  CalorieCook
+//
+//  Created by Tolga Sarikaya on 16.07.24.
+//
+
+import SwiftUI
+
+@main
+struct CalorieCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieCook/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  CalorieCook
+//
+//  Created by Tolga Sarikaya on 16.07.24.
+//
+
+import Foundation
+
